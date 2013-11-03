c51155d800ec651a1c83fc0eebd052f06ba63041
Nate Smith <nate@nateps.com>
2013-11-03 15:31:37 -0800

    don't require createPathExpression by default

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+// createPathExpression is not required here, since it should be an optional
+// include on the client
 exports.contexts = require('./lib/contexts');
 exports.expressions = require('./lib/expressions');
 exports.operatorFns = require('./lib/operatorFns');
-exports.createPathExpression = require('./lib/createPathExpression');
