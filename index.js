ef4bc6e7f20d79d17e804ad348e9edb7404c6598
Nate Smith <nate@nateps.com>
2014-01-04 19:43:34 -0800

    define derby templates in this module

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 exports.contexts = require('./lib/contexts');
 exports.expressions = require('./lib/expressions');
 exports.operatorFns = require('./lib/operatorFns');
+exports.templates = require('./lib/templates');
