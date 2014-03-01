752f98fa6ea81087f81dde66036e82f84a5cb6ed
Nate Smith <nate@nateps.com>
2014-03-01 11:45:36 -0800

    remove outdated comment

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// createPathExpression is not required here, since it should be an optional
-// include on the client
 exports.contexts = require('./lib/contexts');
 exports.expressions = require('./lib/expressions');
 exports.operatorFns = require('./lib/operatorFns');
