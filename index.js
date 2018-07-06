ab529336f691a17e0fd75439b066c59ff1b01b8a
Nate Smith <nate@nateps.com>
2018-07-05 17:09:38 -0700

    implement DependencyOptions & ignoreTemplate option
    Switching dependencies to take a second options argument. The ignoreTemplate option will be used for avoiding looking for dependencies within a given template.
    
    This is useful when a template is going to be rendered separately, since it will emit its own dependencies. This happens in the case of a DynamicText whose value ends up being a template. In this case, the template itself is rendered in the postiion of the dynamic text, and we don't need to look for dependencies within the template at the time of rendering the DynamicText.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.contexts = require('./lib/contexts');
 exports.expressions = require('./lib/expressions');
 exports.operatorFns = require('./lib/operatorFns');
+exports.options = require('./lib/options');
 exports.templates = require('./lib/templates');
